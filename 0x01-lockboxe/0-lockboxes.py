fab1b7d16434df36e274f167f38e0406c7262fa5
velvetvi123 <vic_velvet@yahoo.com>
2024-10-10 12:03:19 -0700

    Done

---
+++ 0x01-lockboxe/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module for the canUnlockAll function.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened.
+
+    Args:
+    boxes (list of lists): A list of lists where each inner list represents
+                           a box and contains keys to other boxes.
+
+    Returns:
+    bool: True if all boxes can be opened, False otherwise.
+    """
+    n = len(boxes)
+    unlocked = set([0])  # The first box is always unlocked
+    keys = set(boxes[0])  # Keys from the first box
+
+    while keys:
+        new_key = keys.pop()
+        if new_key < n and new_key not in unlocked:
+            unlocked.add(new_key)
+            keys.update(boxes[new_key])
+
+    return len(unlocked) == n
