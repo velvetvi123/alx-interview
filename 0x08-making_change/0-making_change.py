78fd32d84ccc13a5fdd556b7fdc09ffb9b2acb06
velvetvi123 <vic_velvet@yahoo.com>
2024-11-28 11:35:01 -0800

    Done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
-"""Module to solve the Making Change Problem."""
+'''DSA Challenge
+'''
 
 
-def make_change(coins, total):
-    """
-    Determines the fewest number of coins needed to meet a given amount total.
-
-    Args:
-        coins (list): List of coin denominations.
-        total (int): The target amount to achieve using the coins.
-
-    Returns:
-        int: The fewest number of coins required, or -1 if it cannot be done.
-    """
+def makeChange(coins, total):
+    '''determine the fewest number of coins needed to meet a given amount total
+    '''
     if total <= 0:
         return 0
-
-    accumulated_total = 0
-    coins_used = 0
-    coins = sorted(coins, reverse=True)
-
-    for coin in coins:
-        num_coins = (total - accumulated_total) // coin
-        accumulated_total += num_coins * coin
-        coins_used += num_coins
-        if accumulated_total == total:
-            return coins_used
-
-    return -1
-
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while total > 0:
+        if coin_idx >= n:
+            return -1
+        if total - sorted_coins[coin_idx] >= 0:
+            total -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
